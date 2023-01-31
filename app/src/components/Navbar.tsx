ac7f69cf61cdeff6c82d36229ceb470cc520b80f
Jimmy <mimiouyang@hotmail.com>
2023-01-31 01:24:54 -0500

    Created navbar (with google form)

---
+++ app/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+function Navbar() {
+	return (
+		<AppBar position="static">
+			<Toolbar>
+				<Button
+					sx={{ color: "white" }}
+					href="https://forms.gle/KokrUFayNYeAoqdMA"
+					target="_blank"
+				>
+					Contact (questions, suggestions, bugs)
+				</Button>
+			</Toolbar>
+		</AppBar>
+	)
+}
+
+export default Navbar
